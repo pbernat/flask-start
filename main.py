76aad650f741434f81653975ad091831f67a49e0
Joel Viellepeau <viellepeau@gmail.com>
2015-05-24 07:11:48 +0200

    base.html and profile.html added

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 
 #import logging
 from google.appengine.api import users
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+app.jinja_env.line_statement_prefix = '#'
 
 @app.route('/', methods=['GET'])
 def index():
@@ -19,7 +21,19 @@ def index():
     ma_variable_integer = 4
     ma_variable_string  = "bonjour"
 
-    return render_template('index.html', toto_int = ma_variable_integer, tata_str = ma_variable_string, **locals())
+    return render_template('index.html', toto = ma_variable_integer, tata = ma_variable_string, **locals())
+
+
+@app.route('/profile', methods=['GET'])
+def user_profile():
+    # Remarque : Nous avons forçé l'utilisateur à se connecter avant d'accéder à cette fontion > cf. fichier app.yaml >> 'login: required'
+
+    user        = users.get_current_user()
+    is_admin    = users.is_current_user_admin()
+    login_url   = users.create_login_url(request.path)
+    logout_url  = users.create_logout_url(request.path)
+
+    return render_template('profile.html', **locals())
 
 
 @app.route('/admin/dashboard', methods=['GET'])
